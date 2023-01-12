e3c6a50fe0cc06e28fda0a546de20a55c8b00578
DESKTOP-M3PV9A5\madhu <mpentakota@vivifyhealthcare.com>
2023-01-12 12:15:46 +0530

    mongo2Mongo

--- mongo2Mongo.py
+++ mongo2Mongo.py
@@ -1,16 +1,26 @@
 from pymongo import MongoClient
 import pymongo
 import urllib.parse
+
+
+Username = input('Enter the UserName: ')
+Password = input('Enter the Password: ')
+from_database = input('Enter the  From database Name:')
+to_host = input('Enter host: ')
+to_database = input('Enter the  To database Name: ')
+
+
 try:
     y = []
-    mongodb_dbname2 = "madhu"
-    username = urllib.parse.quote_plus('devops_admin')
-    password = urllib.parse.quote_plus('Devj0019ej')
+    mongodb_dbname2 = from_database
+    username = urllib.parse.quote_plus(Username)
+    password = urllib.parse.quote_plus(Password)
     myclient2 = MongoClient('mongodb://%s:%s@3.108.11.120:27017' % (username, password))
     mydb2 = myclient2[mongodb_dbname2]
     migration = mydb2.list_collection_names()
-    mongodb_host = "mongodb://localhost:e7017/"
-    mongodb_dbname1 = "lifeeazydatabase"
+
+    mongodb_host = to_host  #"mongodb://localhost:27017/"
+    mongodb_dbname1 = to_database
     myclient1 = pymongo.MongoClient(mongodb_host)
     mydb1 = myclient1[mongodb_dbname1]
     print(myclient1)
